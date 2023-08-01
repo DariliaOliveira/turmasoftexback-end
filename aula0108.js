7a89e34d2a9754d2921690b0f66aa7120b2cbefc
Darília Oliveira <dariliaoliveira@gmail.com>
2023-08-01 12:59:53 +0000

    atualizar010823

---
+++ aula0108.js
@@ -0,0 +1,21 @@
+
+const meuArray = [10,20,30,40,50];
+var segundoElemento = meuArray[1];
+console.log(segundoElemento);
+
+const frutas = ['maçã','banana','laranja','uva','manga'];
+console.log(frutas.length);
+frutas.push('pera');
+console.log(frutas.length);
+console.log(frutas);
+frutas.shift();
+console.log(frutas);
+
+const numeros =[10,20,30,40,50];
+console.log(numeros.includes(25));
+
+
+
+
+
+
