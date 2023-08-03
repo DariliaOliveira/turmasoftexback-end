96aac91654c1b851b9f6d042cd790571796e16ee
Darília Oliveira <dariliaoliveira@gmail.com>
2023-08-03 14:24:14 +0000

    atualizar

--- aula2507.js
+++ aula2507.js
@@ -8,5 +8,151 @@ if (idade < 16){
     console.log("Você pode dirigir.")
 }
 
+dia = 5;
+nomedia = "";
 
+switch(dia) {
+    case 1:
+        nomedia = "Domingo";
+        break;
+    case 2:
+        nomedia = "Segunda";
+        break;
+    case 3:
+        nomedia = "Terça";
+        break;
+    case 4:
+        nomedia = "Quarta";
+        break;
+    case 5:
+        nomedia = "Quinta";
+        break;
+    case 6:
+        nomedia = "Sexta";
+        break;
+    case 7:
+        nomedia = "Sábado";
+        break;
+    default:
+        nomedia = "Dia inexistente";
+}
+console.log(nomedia);
+
+
+mês = 5;
+nomemês = "";
+
+switch(mês) {
+    case 1:
+        nomemês = "Janeiro";
+        break;
+    case 2:
+        nomemês = "Fevereiro";
+        break;
+    case 3:
+        nomemês = "Março";
+        break;
+    case 4:
+        nomemês = "Abril";
+        break;
+    case 5:
+        nomemês = "Maio";
+        break;
+    case 6:
+        nomemês = "Junho";
+        break;
+    case 7:
+        nomemês = "Julho";
+        break;
+    case 8:
+        nomemês = "Agosto";
+        break;
+    case 9:
+        nomemês = "Setembro";
+        break;
+    case 10:
+        nomemês = "Outubro";
+        break;
+    case 11:
+        nomemês = "Novembro";
+        break;
+    case 12:
+        nomemês = "Dezembro";
+        break;
+    default:
+        nomemês = "Mês inexistente";
+}
+console.log(nomemês);
+
+
+
+
+estadocivil = "solteiro";
+msgestadocivil = "";
+
+switch(estadocivil) {
+    case "solteiro":
+        msgestadocivil = "Seu estado civil é SOLTEIRO";
+        break;
+    case "casado":
+        msgestadocivil = "Seu estado civil é CASADO";
+        break;
+    case "divorciado":
+        msgestadocivil = "Seu estado civil é DIVORCIADO";
+        break;
+    case "outro":
+        msgestadocivil = "Estado civil: OUTRO";
+        break;
+    default:
+        msgestadocivil = "Estado civil: OUTRO";
+}
+console.log(msgestadocivil);
+
+
+
+
+animal = "cachorro";
+msganimal = "";
+
+switch(animal) {
+    case "cachorro":
+        msganimal = "Esse animal late.";
+        break;
+    case "gato":
+        msganimal = "Esse animal mia.";
+        break;
+    case "pássaro":
+        msganimal = "Esse animal voa.";
+        break;
+    case "outro":
+        msganimal = "Esse animal não é nem cachorro, nem gato, nem pássaro.";
+        break;
+    default:
+        msganimal = "Esse animal não é nem cachorro, nem gato, nem pássaro.";
+}
+console.log(msganimal);
+
+
+
+
+let num1 = 2, num2 = 1;
+operacao = "divisao";
+
+switch(operacao) {
+    case "adicao":
+        operacao = "A soma é " + (num1 + num2);
+        break;
+    case "subtracao":
+        operacao = "A subtracao é " + (num1 - num2);
+        break;
+    case "multiplicacao":
+        operacao = "A multiplicacao é " + (num1 * num2);
+        break;
+    case "divisao":
+        operacao = "A divisao é " + (num1 / num2);
+        break;
+    default:
+        operacao = "Não há operação";
+}
+console.log(operacao);
 
