181a43f935630828a202b6273bddb40aebd72b08
Darília Oliveira <dariliaoliveira@gmail.com>
2023-08-22 13:34:11 +0000

    atualização

---
+++ aula2208.js
@@ -0,0 +1,37 @@
+// var num = [3, 7, 2, 9,5];
+
+// num.forEach(function (item, indice, array) {
+//     console.log(item, indice);
+// });
+
+// var num = [3, 7, 2, 9,5];
+// for (var i = 0; i < num.length; i++) {
+//     num.forEach(function (item, indice, array) {
+//     console.log(item, indice);
+// })
+// break;
+// };
+
+// var obj = { a: 10, b: 20, c: 30, d:40 };
+// var soma = 0;
+
+//  for (index in obj) {
+//     soma = soma + obj[index];
+// }
+// console.log(soma);
+
+// let num = [12, 5, 8, 21, 16, 7];
+// let resultado = [];
+// for (let index in num){
+//     if(num[index] % 2 === 0){
+//         resultado.push(num[index])
+//     } 
+// } console.log(resultado)
+
+let obj = { alice: 18, bob: 20, carol: 19, david: 21};
+let idade = [];
+for (let i in obj) {
+    if(obj[i] == 19){
+        console.log(`${i}`,`${obj[i]}`);
+    }
+} 
