1f9729451a02cd607e0672c8f57c8928bfdf8a6c
Darília Oliveira <dariliaoliveira@gmail.com>
2023-08-24 13:12:44 +0000

    atualização

---
+++ aula2408.js
@@ -0,0 +1,98 @@
+//1
+
+// var obj = {};
+
+// console.log(obj);
+
+
+//2
+
+// var pessoa ={
+//   nome : "João",
+// }
+// console.log(pessoa.nome);
+
+
+//3
+
+// var usuario ={
+//     idade : 24,
+//     nome : "João",
+// }
+// console.log(usuario.idade);
+
+
+//4
+
+// var produto ={
+//     preco : 50.00,
+// }
+
+// produto.preco = 75.00,
+
+// console.log(produto.preco);
+
+//5
+
+// var contato = {
+//     "nome" : "exemplo",
+//     "email" : "exemplo@email.com.br",
+// }
+
+// delete contato.email;
+
+// console.log(contato);
+
+//6
+
+// var cliente ={
+//     "nome" : "exemplo",
+//     "idade" : 29,
+// }
+
+// console.log(cliente);
+
+// cliente.endereco = "Rua A, 123.";
+
+// console.log(cliente);
+
+//7
+
+// var empresa ={
+//     departamentos : ["financeiro", "contabilidade", "rh"],
+// }
+// console.log(empresa.departamentos[0]);
+
+//8
+
+// var aluno = {
+//     notas : [85,90,78],
+// }
+// console.log(aluno);
+
+// aluno.notas[1] = 95;
+
+// console.log(aluno);
+
+//9
+
+// var dados = {
+//     valores : [10,20,30],
+// }
+// console.log(dados);
+
+// dados.valores.splice(1,1);
+
+// console.log(dados);
+
+//10
+
+var objeto = {
+    "título" : "Livro",
+    "autor" : "Maria",
+}
+console.log(objeto);
+
+
+
+
